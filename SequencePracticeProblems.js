62beaa87ab001d6ffd999418c9689933a039da03
Pavan699 <pavannakate03@gmail.com>
2021-10-21 10:39:29 -0700

    Print dice number from 1 to 6

--- SequencePracticeProblems.js
+++ SequencePracticeProblems.js
@@ -4,3 +4,8 @@ console.log("Sequence Practice Problems...");
 console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 let single_Digit = Math.floor(Math.random() * 10); 
 console.log("Single Digit number is : "+single_Digit);
+
+//Problem-2: Print a dice number from 1-6 using random
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+let Dice = Math.floor(Math.random() * 6 ) + 1;
+console.log("Dice Number is : "+Dice);
