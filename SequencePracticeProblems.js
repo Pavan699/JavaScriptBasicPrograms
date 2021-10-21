cbc44195ab19cd25b6e638bd0e96ab67d2ede1d8
Pavan699 <pavannakate03@gmail.com>
2021-10-21 10:47:30 -0700

    Unit conversion and area calculation

--- SequencePracticeProblems.js
+++ SequencePracticeProblems.js
@@ -31,3 +31,24 @@ let Sum = Random1+Random2+Random3+Random4+Random5;
 console.log("Sum of Five Numbers is : "+Sum);
 let average = Sum / 5;
 console.log("Average of Five numbers is : "+average);
+
+//Problem-5: Unit Conversion
+//Conversion of Inches into feets
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+let One_Feet = 12;//1 feet = 12 inches
+let One_Meter = 3.28;//1 meter = 3.28 feet
+let Total_Plots = 25;
+let feet = 42 / One_Feet;
+console.log("42 inches in feet is : "+feet);
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+//Conversion of Rectanguler plot from feets to meters
+let length = 60;//feets
+let width = 40;//feets
+console.log("Length and Width are "+length+" and "+width);
+let Area_Feets = length * width;
+console.log("Area of Plot in feets is : "+Area_Feets);
+let Area_In_Meter = Area_Feets / One_Meter;
+console.log("Area of Plot in Meters is : "+Area_In_Meter);
+let Total_Area = Area_In_Meter * Total_Plots;
+console.log("Total Area of "+Total_Plots+" Plots is "+Total_Area);
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
