362820f325d01eb4b108d4442a93ee6695a8bc18
Pavan699 <pavannakate03@gmail.com>
2021-10-21 10:35:34 -0700

    Printing Single digit

--- SequencePracticeProblems.js
+++ SequencePracticeProblems.js
@@ -1,3 +1,6 @@
 console.log("Sequence Practice Problems...");
 
-
+//Problem-1: Print a single digit number
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+let single_Digit = Math.floor(Math.random() * 10); 
+console.log("Single Digit number is : "+single_Digit);
