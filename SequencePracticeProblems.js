14953594ee664336be9d6098d92565311c408dd2
Pavan699 <pavannakate03@gmail.com>
2021-10-21 10:44:32 -0700

    sum and avg of five Two digit numbers

--- SequencePracticeProblems.js
+++ SequencePracticeProblems.js
@@ -15,6 +15,19 @@ console.log("Dice Number is : "+Dice);
 console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 let Dice1 = Math.floor(Math.random() * 6 ) + 1;
 let Dice2 = Math.floor(Math.random() * 6 ) + 1;
-console.log("Numbers Are : "+Dice1+" And "+Dice2);
+console.log("Dice Numbers Are : "+Dice1+" And "+Dice2);
 let Result = Dice1 + Dice2;
 console.log("Addition of Two random Dice is : "+Result);
+
+//Problem-4: Write a program that reads 5 Random 2 Digit values , then find their sum and the average
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+let Random1 = Math.floor(Math.random() * 90 )+ 10;
+let Random2 = Math.floor(Math.random() * 90 )+ 10;
+let Random3 = Math.floor(Math.random() * 90 )+ 10;
+let Random4 = Math.floor(Math.random() * 90 )+ 10;
+let Random5 = Math.floor(Math.random() * 90 )+ 10;
+console.log("Two digits Numbers are : "+Random1+","+Random2+","+Random3+","+Random4+","+Random5)
+let Sum = Random1+Random2+Random3+Random4+Random5;
+console.log("Sum of Five Numbers is : "+Sum);
+let average = Sum / 5;
+console.log("Average of Five numbers is : "+average);
