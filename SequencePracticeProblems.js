adeee07eec412050a7a74084f63cca94594ed475
Pavan699 <pavannakate03@gmail.com>
2021-10-21 10:41:19 -0700

    Addition of two dice numbers

--- SequencePracticeProblems.js
+++ SequencePracticeProblems.js
@@ -9,3 +9,12 @@ console.log("Single Digit number is : "+single_Digit);
 console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 let Dice = Math.floor(Math.random() * 6 ) + 1;
 console.log("Dice Number is : "+Dice);
+
+
+//Problem-3: Add two Random Dice Number and Print the Result
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+let Dice1 = Math.floor(Math.random() * 6 ) + 1;
+let Dice2 = Math.floor(Math.random() * 6 ) + 1;
+console.log("Numbers Are : "+Dice1+" And "+Dice2);
+let Result = Dice1 + Dice2;
+console.log("Addition of Two random Dice is : "+Result);
