64cd7954e78032a37dc2785d111b66c0086a7d15
Pavan699 <pavannakate03@gmail.com>
2021-10-22 07:40:53 -0700

    Flip Coin with If Else using Random

---
+++ IfElse.js
@@ -0,0 +1,10 @@
+console.log("If-Else Practice Problems.....")
+//Flip Coin Print Heads or Tails
+let Coin = Math.floor(Math.random() * 2);
+if(Coin == 0)
+{
+    console.log("Tails");
+}else
+{
+    console.log("Heads");
+}
